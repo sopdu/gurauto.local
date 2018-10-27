3d7315ede8a16b6ba0a9aa37fbf1fc8ce470fd7f
sopdu <admin@sopdu.ru>
2018-10-27 13:06:59 +0300

    Смотрим, что содержит элемент каталога

--- api/test.php
+++ api/test.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 27.10.2018
- * Time: 12:49
- */
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+# тестовые наборы данных
+$section = 'Насосы гидроусилителя руля ГУР (новые)';
+$idTesting = 772734;
+
+/*
+Насос ГУР ACURA MDX (YD2) 3.7 AWD
+*/
+
+# смотрим, что содержит элемент каталога
+if (
+    CModule::IncludeModule("catalog") and
+    CModule::IncludeModule("iblock")
+){
+    $zaprosElement = CCatalogProduct::GetByID($idTesting);
+    $ilsResult = $zaprosElement;
+}
+
+if(!empty($ilsResult)){
+    echo '<pre style="color:green">'; print_r($ilsResult); '</pre>';
+}
+?>
